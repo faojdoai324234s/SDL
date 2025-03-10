72987b0752029a66d5227c51f697c40bab959915
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-10 10:01:22 +0000

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -10,10 +10,10 @@ mkdir upload/Release
 git clone --branch release-3.2.x --single-branch https://github.com/libsdl-org/SDL
 
 # Install dependencies
-sudo apt-get update && sudo apt-get install libglew-dev pulseaudio
+sudo apt-get update && sudo apt-get install libglew-dev libpulse-dev libpipewire-1.4-0
 
 # Build Debug configuration
-cmake -S SDL -B build -D CMAKE_BUILD_TYPE=Debug -D SDL_CAMERA=OFF -D SDL_JOYSTICK=OFF -D SDL_HAPTIC=OFF -D SDL_HIDAPI=OFF -D SDL_POWER=OFF -D SDL_SENSOR=OFF -D SDL_DIALOG=OFF -D SDL_PULSEAUDIO_SHARED=ON
+cmake -S SDL -B build -D CMAKE_BUILD_TYPE=Debug -D SDL_CAMERA=OFF -D SDL_JOYSTICK=OFF -D SDL_HAPTIC=OFF -D SDL_HIDAPI=OFF -D SDL_POWER=OFF -D SDL_SENSOR=OFF -D SDL_DIALOG=OFF
 cmake --build build --config Debug
 cmake --install build
 
@@ -24,7 +24,7 @@ cp build/SDL3.so upload/Debug/SDL3d.so
 rm -rf build
 
 # Build Release configuration
-cmake -S SDL -B build -D CMAKE_BUILD_TYPE=Release -D SDL_CAMERA=OFF -D SDL_JOYSTICK=OFF -D SDL_HAPTIC=OFF -D SDL_HIDAPI=OFF -D SDL_POWER=OFF -D SDL_SENSOR=OFF -D SDL_DIALOG=OFF -D SDL_PULSEAUDIO_SHARED=ON
+cmake -S SDL -B build -D CMAKE_BUILD_TYPE=Release -D SDL_CAMERA=OFF -D SDL_JOYSTICK=OFF -D SDL_HAPTIC=OFF -D SDL_HIDAPI=OFF -D SDL_POWER=OFF -D SDL_SENSOR=OFF -D SDL_DIALOG=OFF
 cmake --build build --config Release
 cmake --install build
 
