7de4520c44ebce3981a54993a2c6e9967e371d49
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-10 09:27:11 +0000

    Create build_linux.sh

---
+++ build_linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Prepare the build with creating the file structure
+mkdir upload
+mkdir upload/include
+mkdir upload/include/sdl
+mkdir upload/Debug
+mkdir upload/Release
+
+git clone --branch release-3.2.x --single-branch https://github.com/libsdl-org/SDL
+
+# Build Debug configuration
+cmake -S SDL -B build -D CMAKE_BUILD_TYPE=Debug -D SDL_CAMERA=OFF -D SDL_JOYSTICK=OFF -D SDL_HAPTIC=OFF -D SDL_HIDAPI=OFF -D SDL_POWER=OFF -D SDL_SENSOR=OFF -D SDL_DIALOG=OFF
+cmake --build build --config Debug
+cmake --install build
+
+# Copy over the built files
+cp build/SDL3.so upload/Debug/SDL3d.so
+
+# Clean up before we run CMake again
+rm -rf build
+
+# Build Release configuration
+cmake -S SDL -B build -D CMAKE_BUILD_TYPE=Release -D SDL_CAMERA=OFF -D SDL_JOYSTICK=OFF -D SDL_HAPTIC=OFF -D SDL_HIDAPI=OFF -D SDL_POWER=OFF -D SDL_SENSOR=OFF -D SDL_DIALOG=OFF
+cmake --build build --config Release
+cmake --install build
+
+# Copy over the built files
+cp build/SDL3.so upload/Release
+
+# Copy over the headers
+cp -r SDL/include/SDL3/. upload/include/sdl
