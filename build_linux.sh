68d1ea3a5e54796cdc03d74fb4991a9828b1a0db
faojdoai324234s <faojdoai324234s@protonmail.com>
2025-03-10 10:09:34 +0000

    Update build_linux.sh

--- build_linux.sh
+++ build_linux.sh
@@ -10,7 +10,7 @@ mkdir upload/Release
 git clone --branch release-3.2.x --single-branch https://github.com/libsdl-org/SDL
 
 # Install dependencies
-sudo apt-get update && sudo apt-get install libglew-dev libpulse-dev libpipewire-0.3
+sudo apt-get update && sudo apt-get install libglew-dev libasound2-dev libpulse-dev libpipewire-0.3
 
 # Build Debug configuration
 cmake -S SDL -B build -D CMAKE_BUILD_TYPE=Debug -D SDL_CAMERA=OFF -D SDL_JOYSTICK=OFF -D SDL_HAPTIC=OFF -D SDL_HIDAPI=OFF -D SDL_POWER=OFF -D SDL_SENSOR=OFF -D SDL_DIALOG=OFF -D SDL_PULSEAUDIO=ON -D SDL_PIPEWIRE=ON
